39867517c050db4d1d85fe637e7185b2bed46190
Sasindu Shalika <MXsasindushalika001@gmail.com>
2024-11-21 22:49:35 +0530

    create user model

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Post = require('./models/Post');
+const Post = require('./models/User');
 const multer = require('multer');
 
 
@@ -8,11 +9,14 @@ const app = express();
 
 
 //const port = 5000;
+//const mongourl='mongodb://localhost:0000/';
+
+
 app.listen(port, () => {
     console.log("Backend is running on port", port);
 });
 
-//const mongourl='-------------';
+
 mongoose.connect(mongourl).then(() => {
     console.log('MongoDB Connected');
 }).catch((error) => {
---
+++ backend/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    name: {type: String, required: true,},
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    profile_picture: {type: String, required: false},
+    role: {type: String, required: true}
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
