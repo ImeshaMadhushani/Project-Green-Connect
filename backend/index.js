8ada5c33c5a4b08f00ef663624f2b27ca9892380
Sasindu Shalika <MXsasindushalika001@gmail.com>
2024-12-01 07:42:19 +0530

    add session management

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 //import Post from './models/Post.js';
 import dotenv from 'dotenv';
+const session = require('express-session');
 
 dotenv.config();
 const app = express();
+
+//session management
+
+app.use(session({
+    secret: 'yourSecretKey', 
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } 
+  }));
 
 
 // Connect to MongoDB
