bf3aa6319ab89e41fd2ff0050192c179b7a3c37f
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2024-12-04 17:59:06 +0530

    fix error in parse JSON in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 import userRouter from './routes/userRoute.js';
 
 dotenv.config();
 const app = express();
 
+app.use(bodyParser.json());
 
 app.use("/api/user", userRouter);
 
