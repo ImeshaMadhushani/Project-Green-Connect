d0544f21f36ffecd284ad1a823d450e24ec07721
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2024-12-04 17:40:33 +0530

    create user register function on userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,4 +3,31 @@ import User from '../models/User';
 // Register a new user
 export async function register(req, res) {
     const { name, username, email, password, profile_picture, role } = req.body;
+    const saltRound = 10;
+
+    try {
+        const existingUser = await User.findOne({ email });
+        
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email already exists' });
+        }
+
+        const hashedPassword = bcrypt.hashSync(password, saltRound);
+
+        const newUser = new User({
+            name,
+            username,
+            email,
+            password: hashedPassword,
+            profile_picture,
+            role
+        });
+
+        await newUser.save()
+        console.log("Recieved new user : ", req.body);
+        
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (err) { 
+        res.status(500).json({ message: "Error creating user!", error: error.message });
+    }
 }
