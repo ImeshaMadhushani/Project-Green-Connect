06ee30fb67281041a2c4cd06589ef333cc29d1c3
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2024-12-09 20:26:04 +0530

    Update projectController.js
    Create getProjectById function for get a single project in a project Controller

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -39,3 +39,16 @@ export const getProjects = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+// Get a single project
+export const getProjectById = async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.id);
+        if (!project) {
+            return res.status(404).json({ message: "Project not found" });
+        }
+        res.status(200).json(project);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
