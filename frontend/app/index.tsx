0c1a376d73025f306f490fd530a4c616dc827738
SanduniAmasha <139688263+SanduniAmasha@users.noreply.github.com>
2024-12-14 01:33:54 +0530

    Add a status bar

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,13 @@
 import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import { StatusBar } from 'expo-status-bar';
 const bgImage = require("../assets/images/bg.jpg");
 
 const Index = () => {
   return (
+    
     <ImageBackground style={styles.background} source={bgImage}>
+      <StatusBar backgroundColor='#0B3D2E' style='light' />
+
       <View style={styles.container}>
         <Text>Landing Page</Text>
       </View>
