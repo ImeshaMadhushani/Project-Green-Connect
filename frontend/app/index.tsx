d1fbea8531f035db508b341df12bffbf888cc912
manimekala123 <139643976+manimekala123@users.noreply.github.com>
2024-12-24 11:28:14 +0530

    Finalize button functionality and export

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,16 +1,40 @@
 import { View, Text, StyleSheet, ImageBackground } from "react-native";
 const bgImage = require("../assets/images/bg.jpg");
 import Button from "@/components/button-login";
+const logo = require("../assets/images/logo1.png");
+import { router } from "expo-router";
 
 const Index = () => {
-  return (
+  const ui = (
     <ImageBackground style={styles.background} source={bgImage}>
-      <View style={styles.container}>
-        <Text>Landing Page</Text>
-      </View>
+      
+        <View
+          style={{
+            width: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+       
+          <Text style={{ color: "white" }}>
+            Together We Can Make a Difference
+          </Text>
+            <Button onPress={()=>{
+             
+            }} label="Volunteer" />
+            <Text style={
+              {
+                color:"white"
+              }
+            }>OR</Text>
+            <Button label="Organization" />
+        </View>
+      
     </ImageBackground>
   );
+  return ui;
 };
+  
 
 const styles = StyleSheet.create({
   background: {
