2d3429bc49e21bdfea889666cb6cbd1b1aff32ff
SanduniAmasha <139688263+SanduniAmasha@users.noreply.github.com>
2024-12-26 11:31:44 +0530

    Update index.tsx
    add tagline styles

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 import { View, Text, StyleSheet, Image, ImageBackground } from "react-native";
 import { StatusBar } from 'expo-status-bar';
+=======
+import { View, Text, StyleSheet, ImageBackground , Image, StatusBar} from "react-native";
+>>>>>>> Stashed changes
 const bgImage = require("../assets/images/bg.jpg");
 const logo = require("../assets/images/logo1.png");
 
@@ -7,12 +11,36 @@ const Index = () => {
   return (
     
     <ImageBackground style={styles.background} source={bgImage}>
+<<<<<<< Updated upstream
       <StatusBar backgroundColor='#0B3D2E' style='light' />
 
       <View style={styles.container}>
         <Image style={styles.logo} resizeMode="contain" source={logo} />
         <Text>Landing Page</Text>
       </View>
+=======
+      
+      <View style={styles.container}>
+        <StatusBar backgroundColor="#1F4529" barStyle="light-content" />
+
+          <Image style={styles.logo} resizeMode="contain" source={logo} />
+       
+          <Text style={styles.tagline}>
+            Together We Can Make a Difference
+          </Text>
+            <Button onPress={()=>{
+             
+            }} label="Volunteer" />
+            <Text style={
+              {
+                color:"white",
+                fontSize: 16
+              }
+            }>OR</Text>
+            <Button label="Organization" />
+        </View>
+      
+>>>>>>> Stashed changes
     </ImageBackground>
   );
 };
@@ -28,9 +56,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   logo: {
+<<<<<<< Updated upstream
     width: "50%",
     height: 250,
     marginBottom: 25,
+=======
+    width: "60%",
+    height: 250,
+    marginBottom: 20,
+  },
+  tagline: {
+    color: "white",
+    fontSize: 20,
+    textAlign: "center",
+    marginBottom: 20,
+>>>>>>> Stashed changes
   },
 });
 
