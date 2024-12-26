bc8f2aca41bd5a213d470b170796dd50e753b0f4
Imesha Madhumali <imeshamadhumali31@gmail.com>
2024-12-26 15:42:48 +0530

    Initial setup for TextInputStyled component

---
+++ frontend/components/test-input.tsx
@@ -0,0 +1,38 @@
+import React, { forwardRef, useState } from "react";
+import { TextInput, TextInputProps, StyleSheet, Text } from "react-native";
+
+type Props = TextInputProps & {
+  value?: string;
+  text?: string;
+  password?: boolean;
+};
+
+const TextInputStyled = forwardRef<TextInput, Props>(({ password = false, placeholder, text, ...props }, ref) => {
+    return (
+    <>
+    <Text style={{textAlign:'left',fontSize:15,width:"100%"}}>{text}</Text>
+    <TextInput 
+      ref={ref}
+      placeholder={placeholder}
+      {...props}  
+      style={styles.input}
+    />
+    </>
+  );
+});
+
+const styles = StyleSheet.create({
+  input: {
+    borderRadius: 5,
+    borderColor: '#ccc',
+    borderWidth: 2,
+    height: 50,
+    marginTop: 10,
+    marginBottom: 10,
+    paddingLeft: 10, 
+    fontSize: 18,
+    width: "100%"
+  }
+});
+
+export default TextInputStyled;
