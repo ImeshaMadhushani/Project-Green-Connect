443d72834486e94dc40c39f74cabcf1b64a9a009
Chesintha <139671816+Chesintha@users.noreply.github.com>
2024-12-28 01:35:08 +0530

    added comment routes to postRoute

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import multer from "multer";
 import { createPost, deletePost, updatePost } from "../controllers/postController/postController";
+import { addComment, deleteComment } from "../controllers/postController/commentController";
 
 
 const upload = multer({ dest: "uploads/" });
@@ -10,5 +11,7 @@ const router = Router();
 router.post("/", upload.single("image"), createPost);
 router.post("/:id/delete", deletePost);
 router.post("/:id/update", upload.single("image"), updatePost);
+router.post("/:id/comment", addComment);
+router.post("/:postId/comment/:commentId/delete", deleteComment);
 
 export default router;
