d0ff6a416d0b99c4e01af2dccebc0df3b0272a7b
manimekala123 <139643976+manimekala123@users.noreply.github.com>
2024-12-27 08:45:43 +0530

    Add UI structure and styles

--- frontend/app/(signInsignup)/(ngo)/firstPage/index.tsx
+++ frontend/app/(signInsignup)/(ngo)/firstPage/index.tsx
@@ -11,7 +11,59 @@ const First = () => {
   const input2Ref = useRef<TextInput>(null);
   const input3Ref = useRef<TextInput>(null);
 
-  return <View></View>;
+  const ui = (
+    <View
+      style={{
+        flex: 1,
+        width: "100%",
+        padding: 20,
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: 10,
+          margin: 10,
+        }}
+      >
+        <View
+          style={{
+            width: 15,
+            height: 15,
+            borderColor: "#0D7C66",
+            borderWidth: 2,
+            borderRadius: "100%",
+            backgroundColor: "#0D7C66",
+          }}
+        />
+        <View
+          style={{
+            width: 15,
+            height: 15,
+            borderColor: "#0D7C66",
+            borderWidth: 2,
+            borderRadius: "100%",
+          }}
+        />
+        <View
+          style={{
+            width: 15,
+            height: 15,
+            borderColor: "#0D7C66",
+            borderWidth: 2,
+            borderRadius: "100%",
+          }}
+        />
+      </View>
+    </View>
+  );
+  
+  return ui;
+  
+
+
 };
 
 export default First;
