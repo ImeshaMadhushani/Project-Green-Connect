0efda397f172ce2b7121f1db88727d628326895e
Imesha Madhumali <imeshamadhumali31@gmail.com>
2024-12-28 16:15:40 +0530

    Add state management

--- frontend/app/(signInsignup)/volunteer/login/index.tsx
+++ frontend/app/(signInsignup)/volunteer/login/index.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 
 const LogIn: React.FC = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
   return (
     <View style={{ flex: 1, width: "100%", padding: 20, alignItems: "center" }}>
       <Text style={{ fontSize: 50, marginBottom: 60 }}>Welcome Back</Text>
