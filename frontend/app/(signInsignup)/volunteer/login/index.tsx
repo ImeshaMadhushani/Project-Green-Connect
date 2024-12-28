406f15574cd66b02c7bc8c4857de54b01e033eda
Imesha Madhumali <imeshamadhumali31@gmail.com>
2024-12-28 16:03:56 +0530

    Initialize login component structure

---
+++ frontend/app/(signInsignup)/volunteer/login/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const LogIn: React.FC = () => {
+  return (
+    <View style={{ flex: 1, width: "100%", padding: 20, alignItems: "center" }}>
+      <Text style={{ fontSize: 50, marginBottom: 60 }}>Welcome Back</Text>
+    </View>
+  );
+};
+
+export default LogIn;
