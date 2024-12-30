0bcbefdc2006458fc29efab8159237c9f14ce092
Krishan <kirioffl43@gmail.com>
2024-12-30 21:16:44 +0530

    styles done

--- frontend/app/(signInsignup)/(ngo)/fsecondpage/index.tsx
+++ frontend/app/(signInsignup)/(ngo)/fsecondpage/index.tsx
@@ -19,3 +19,52 @@ const Second = () => {
 
   return null;
 };
+const ui = (
+    <View
+      style={{
+        flex: 1,
+        width: "100%",
+        padding: 20,
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: 10,
+          margin: 10,
+        }}
+      >
+        <View
+          style={{
+            width: 15,
+            height: 15,
+            borderColor: "#0D7C66",
+            borderWidth: 2,
+            borderRadius: "100%",
+          }}
+        />
+        <View
+          style={{
+            width: 15,
+            height: 15,
+            borderColor: "#0D7C66",
+            borderWidth: 2,
+            borderRadius: "100%",
+            backgroundColor: "#0D7C66",
+          }}
+        />
+        <View
+          style={{
+            width: 15,
+            height: 15,
+            borderColor: "#0D7C66",
+            borderWidth: 2,
+            borderRadius: "100%",
+          }}
+        />
+      </View>
+    </View>
+  );
+  
