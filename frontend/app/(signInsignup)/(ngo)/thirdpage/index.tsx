78bf37a763e7f20637c1bd8e8b21441ee697ee1d
Krishan <kirioffl43@gmail.com>
2024-12-30 21:35:24 +0530

    setup commponents structure

---
+++ frontend/app/(signInsignup)/(ngo)/thirdpage/index.tsx
@@ -0,0 +1,24 @@
+import ButtonGoogle from "@/components/button-google";
+import ButtonSuccess from "@/components/button-success";
+import ButtonText from "@/components/button-text";
+import TextInputStyled from "@/components/text-input";
+import { router } from "expo-router";
+import { useRef, useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { CheckBox } from "react-native-elements/dist/checkbox/CheckBox";
+import { TextInput } from "react-native-gesture-handler";
+
+const Third = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+
+  const input1Ref = useRef<TextInput>(null);
+  const input2Ref = useRef<TextInput>(null);
+  const input3Ref = useRef<TextInput>(null);
+
+  return null;
+};
+
+export default Third;
