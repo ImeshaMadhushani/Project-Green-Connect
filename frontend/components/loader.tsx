3dea8bbe9d1c6426f1fb85a60bfb0b5ff05ffdda
SanduniAmasha <139688263+SanduniAmasha@users.noreply.github.com>
2024-12-30 11:39:02 +0530

    Add some styles to loader and inner container

--- frontend/components/loader.tsx
+++ frontend/components/loader.tsx
@@ -2,7 +2,28 @@ import { Image } from "expo-image";
 import { View } from "react-native";
 
 const Loader = () => {
-  const ui = <View></View>;
+  const ui =  (
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        backgroundColor: "#ffffffad",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View style={{
+          width: 150,
+          height: 150,
+          borderRadius: 20,
+          backgroundColor: "white",
+        }}>
+            
+        </View>
+    </View>
+  );
+
   return ui;
 };
 
