a1df9a0fa29eaac94be17a7839d3b638fafb3cc3
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-02 05:27:27 +0530

    Import statements and asset requirements

---
+++ frontend/app/(tabs)/news.tsx
@@ -0,0 +1,16 @@
+import Card from "@/components/Card";
+import { router, Stack } from "expo-router";
+import {
+  Alert,
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+
+const plus = require("../../assets/images/plus.png");
+const scope = require("../../assets/images/scope.png");
