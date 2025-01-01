f09807c2902cfef42afbdc0b75cdddfcbfdef190
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-01 17:02:02 +0530

    Implement UI component to signUp componenet

--- frontend/app/(signInsignup)/volunteer/signUp/index.tsx
+++ frontend/app/(signInsignup)/volunteer/signUp/index.tsx
@@ -7,6 +7,107 @@ import { useRef, useState } from "react";
 import { Alert, StyleSheet, Text, View } from "react-native";
 import { CheckBox } from "react-native-elements/dist/checkbox/CheckBox";
 import { TextInput } from "react-native-gesture-handler";
+import { markError, markOk } from "./SignUpUtils";
+
+const ui = (
+  <View
+    style={{
+      flex: 1,
+      width: "100%",
+      padding: 20,
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+  >
+    <TextInputStyled
+      ref={input1Ref}
+      returnKeyType="next"
+      text="E-mail"
+      onSubmitEditing={() => {
+        if (email == null || email == "") {
+          markError(input1Ref);
+          Alert.alert("Error", "Enter Email");
+          input1Ref.current?.focus();
+        } else {
+          markOk(input1Ref);
+          input2Ref.current?.focus();
+        }
+      }}
+      onChangeText={setEmail}
+      value={email}
+      placeholder={"Enter E-mail"}
+    />
+    <TextInputStyled
+      ref={input2Ref}
+      password={true}
+      returnKeyType="next"
+      text="Password"
+      onSubmitEditing={() => {
+        if (password == null || password == "") {
+          markError(input2Ref);
+          Alert.alert("Error", "Enter Password");
+          input2Ref.current?.focus();
+        } else {
+          markOk(input2Ref);
+          input3Ref.current?.focus();
+        }
+      }}
+      onChangeText={setPassword}
+      value={password}
+      placeholder={"Enter Password"}
+    />
+    <TextInputStyled
+      password={true}
+      ref={input3Ref}
+      text="Confirm Password"
+      returnKeyType="next"
+      onSubmitEditing={() => {
+        if (password2 == null || password2 == "") {
+          markError(input3Ref);
+          Alert.alert("Error", "Enter Password");
+          input3Ref.current?.focus();
+        } else {
+          markOk(input3Ref);
+        }
+      }}
+      onChangeText={setPassword2}
+      value={password2}
+      placeholder={"Enter Password"}
+    />
+    <CheckBox
+      iconType="material-community"
+      checkedIcon="checkbox-marked"
+      uncheckedIcon="checkbox-blank-outline"
+      checkedColor="green"
+      title="by Signing up you agree to our terms & conditions of use and privacy policy"
+      checked={isChecked}
+      onPress={() => setIsChecked(!isChecked)}
+    />
+    <ButtonSuccess
+      label="SIGN UP"
+      onPress={() => {
+        router.navigate("/home", { relativeToDirectory: true })
+      }}
+    />
+
+    <View style={styles.container}>
+      <View style={styles.divider} />
+      <Text style={styles.text}> Or Sign Up With </Text>
+      <View style={styles.divider} />
+    </View>
+
+    <ButtonGoogle onPress={() => {
+        router.navigate("/home", { relativeToDirectory: true })
+      }} />
+      <View style={{
+        flexDirection:'row',
+      }}>
+      <Text style={{fontSize: 20,}}> Alredy Have An Account? </Text><ButtonText label="Login"  onPress={()=>{
+        router.navigate("/logIn", { relativeToDirectory: true })
+      }}/> 
+      </View>
+  </View>
+);
 
 export const markError = (inputRef: React.RefObject<TextInput>) => {
   inputRef.current?.setNativeProps({
