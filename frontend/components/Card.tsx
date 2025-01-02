433220b0b4ff26db378d06ce9720c5884d57b320
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-02 06:35:51 +0530

    Define card component functions

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -8,3 +8,56 @@ type Props = {
   bgColor: string;
   content: React.JSX.Element;
 };
+
+const Card = ({ image, heading, bgColor, content }: Props) => (
+    <View
+      style={{
+        width: "100%",
+        height: "auto",
+        padding: 10,
+      }}
+    >
+      <View
+        style={{
+          width: "100%",
+          height: "auto",
+          backgroundColor: bgColor,
+          borderRadius: 20,
+          padding: 15,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Image
+            style={{
+              height: 100,
+              aspectRatio: 1,
+              borderRadius: 10,
+              marginTop: 15,
+              marginBottom: 15,
+            }}
+            source={image}
+          />
+          <View
+            style={{
+              height: "auto",
+              flex: 1,
+              marginLeft: 15,
+              borderRadius: 10,
+              padding: 10,
+            }}
+          >
+            <Text style={{ fontSize: 20 }}>{heading}</Text>
+            {content}
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+  
