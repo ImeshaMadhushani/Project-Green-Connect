96931f957b795b62f9220733033e381a2d68ffb0
Krishan <kirioffl43@gmail.com>
2025-01-02 19:09:27 +0530

    Add MapView section

--- frontend/app/(tabs)/home.tsx
+++ frontend/app/(tabs)/home.tsx
@@ -1,4 +1,5 @@
 import { ScrollView, StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 
 const Home = () => {
   return (
@@ -18,5 +19,33 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
   },
 });
+
+
+
+const Home = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.contentContainer}>
+      <View style={styles.container}>
+        <View
+          style={{
+            width: "100%",
+            height: 350,
+            marginBottom: 10,
+          }}
+        >
+          <MapView style={styles.map} />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
 
 export default Home;
