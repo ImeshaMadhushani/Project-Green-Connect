a4cac83e5a08dfb81fa3ef93108801381cccebba
SanduniAmasha <139688263+SanduniAmasha@users.noreply.github.com>
2025-01-03 01:25:54 +0530

    resolve some conflicts

--- frontend/app/(signInsignup)/(volunteer)/forgetPassword/index.tsx
+++ frontend/app/(signInsignup)/(volunteer)/forgetPassword/index.tsx
@@ -1,119 +1,3 @@
-<<<<<<< Updated upstream
-import ButtonGoogle from "@/components/button-google";
-import ButtonSuccess from "@/components/button-success";
-import ButtonText from "@/components/button-text";
-import TextInputStyled from "@/components/text-input";
-import { router } from "expo-router";
-import { useRef, useState } from "react";
-import { Alert, StyleSheet, Text, View } from "react-native";
-import { TextInput } from "react-native-gesture-handler";
-//
-const FogetPassword = () => {
-  const [email, setEmail] = useState("");
-  const input1Ref = useRef<TextInput>(null);
-
-  const markError = (inputRef: React.RefObject<TextInput>) => {
-    inputRef.current?.setNativeProps({
-      style: {
-        borderColor: "tomato",
-        borderWidth: 2,
-      },
-    });
-  };
-
-  const markOk = (inputRef: React.RefObject<TextInput>) => {
-    inputRef.current?.setNativeProps({
-      style: {
-        borderColor: "green",
-        borderWidth: 2,
-      },
-    });
-  };
-
-  const ui = (
-    <View
-      style={{
-        flex: 1,
-        width: "100%",
-        padding: 20,
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontSize: 50, marginBottom: 10 }}>Foget Password</Text>
-      <Text style={{ fontSize: 20, marginBottom: 60 }}>
-        Enter Your Email Address To Reset Password
-      </Text>
-      <ButtonSuccess
-        label="NEXT"
-        onPress={() => {
-          router.navigate("/resetPassword", { relativeToDirectory: true });
-        }}
-      />
-    </View>
-  );
-
-  const updatedUI = (
-    <View
-      style={{
-        flex: 1,
-        width: "100%",
-        padding: 20,
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontSize: 50, marginBottom: 10 }}>Foget Password</Text>
-      <Text style={{ fontSize: 20, marginBottom: 60 }}>
-        Enter Your Email Address To Reset Password
-      </Text>
-      <TextInputStyled
-        ref={input1Ref}
-        returnKeyType="next"
-        text="E-mail"
-        onSubmitEditing={() => {
-          if (email == null || email === "") {
-            markError(input1Ref);
-            Alert.alert("Error", "Enter Email");
-            input1Ref.current?.focus();
-          } else {
-            markOk(input1Ref);
-          }
-        }}
-        onChangeText={setEmail}
-        value={email}
-        placeholder={"Enter E-mail"}
-      />
-      <ButtonSuccess
-        label="NEXT"
-        onPress={() => {
-          router.navigate("/resetPassword", { relativeToDirectory: true });
-        }}
-      />
-    </View>
-  );
-
-  const styles = StyleSheet.create({
-    container: {
-      flexDirection: "row",
-      alignItems: "center",
-      width: "100%",
-      paddingHorizontal: 16,
-    },
-    divider: {
-      flex: 1,
-      height: 1,
-      backgroundColor: "#ccc",
-    },
-    text: {
-      marginHorizontal: 8,
-      fontSize: 16,
-      color: "#555",
-    },
-  });
-
-  return ui;
-};
-
-=======
 import ButtonGoogle from "@/components/button-google";
 import ButtonSuccess from "@/components/button-success";
 import ButtonText from "@/components/button-text";
@@ -149,7 +33,7 @@ const FogetPassword = () => {
     <View
       style={styles.container}
     >
-      <Text style={{ fontSize: 40, marginBottom: 20 }}>Forget Password</Text>
+      <Text style={{ fontSize: 40, marginBottom: 20, fontWeight: "bold"}}>Forget Password</Text>
       <Text style={{ fontSize: 20, marginBottom: 60 }}>
         Enter Your Email Address To Reset Password
       </Text>
@@ -172,6 +56,7 @@ const FogetPassword = () => {
       />
       <ButtonSuccess
         label="NEXT"
+        style={styles.button}
         onPress={() => {
           router.navigate("/resetPassword", { relativeToDirectory: true });
         }}
@@ -188,7 +73,7 @@ const styles = StyleSheet.create({
     padding: 20,
     alignItems: "center",
     width: "100%",
-    paddingHorizontal: 16,
+    paddingHorizontal: 30,
   },
   divider: {
     flex: 1,
@@ -200,7 +85,9 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "#555",
   },
+  button:{
+    height:45,
+    width:"50%",
+  }
 });
-
->>>>>>> Stashed changes
 export default FogetPassword;
