25d31389bd73c249475293dab62d0a18c5ffb733
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-02 06:13:17 +0530

    Addstyles to the news component

--- frontend/app/(tabs)/news.tsx
+++ frontend/app/(tabs)/news.tsx
@@ -14,76 +14,169 @@ import {
 const plus = require("../../assets/images/plus.png");
 const scope = require("../../assets/images/scope.png");
 
-// News component function
 const News = () => {
-    // UI of the News component
     const ui = (
       <>
         <ScrollView contentContainerStyle={styles.contentContainer}>
           <View style={styles.container}>
-            <View style={{ width: "100%", padding: 10 }}>
-              <Text style={{ fontSize: 25 }}>Articles/News</Text>
+            <View style={{ 
+                width: "100%", 
+                padding: 10 
+                }}
+                >
+              <Text style={{ 
+                fontSize: 25 
+                }}
+                >
+                    Articles/News
+              </Text>
+              <View style={{
+                padding: 3,
+                margin: 10,
+                borderWidth: 1,
+                borderColor: "black",
+                borderRadius: 20,
+                flexDirection: "row",
+                alignItems: "center",
 
-              <View style={searchBarStyle}>
+              }}
+              >
               <TextInput
                 placeholder="Search"
-                style={{ flex: 1, borderWidth: 0 }}
+                style={{ 
+                    flex: 1, 
+                    borderWidth: 0 
+                }}
               />
-              <Pressable onPress={() => router.navigate('/user')}>
-                <Image source={scope} style={{ width: 30, height: 30 }} />
+              <Pressable onPress={() => {
+                router.navigate('/user')
+              }}>
+                <Image source={scope} style={{ 
+                    width: 30, 
+                    height: 30 
+                    }} 
+                    />
               </Pressable>
             </View>
           </View>
 
-                    {/* Card components for different news articles */}
-                    <Card
+                   
+          <Card
             heading="Energy - Saving Tips for an eco-Friendly Home"
             bgColor="#d6e4e8"
-            content={newsContent("Reducing energy consumption at home not only lowers utility bills but also helps protect the environment. Simple changes...")}
+            content={
+              <View style={styles.cardTextContent}>
+                <Text style={styles.cardText}>
+                  Reducing energy consumption at home not only lowers utility
+                  bills but also helps protect the environment. Simple
+                  changes...
+                </Text>
+              </View>
+            }
           />
-          
+
           <Card
             heading="Energy - Saving Tips for an eco-Friendly Home"
             bgColor="#FFFDEC"
-            content={newsContent("Reducing energy consumption at home not only lowers utility bills but also helps protect the environment. Simple changes...")}
+            content={
+              <View style={styles.cardTextContent}>
+                <Text style={styles.cardText}>
+                  Reducing energy consumption at home not only lowers utility
+                  bills but also helps protect the environment. Simple
+                  changes...
+                </Text>
+              </View>
+            }
           />
-          
+
           <Card
             heading="Energy - Saving Tips for an eco-Friendly Home"
             bgColor="#F9E6CF"
-            content={newsContent("Reducing energy consumption at home not only lowers utility bills but also helps protect the environment. Simple changes...")}
+            content={
+              <View style={styles.cardTextContent}>
+                <Text style={styles.cardText}>
+                  Reducing energy consumption at home not only lowers utility
+                  bills but also helps protect the environment. Simple
+                  changes...
+                </Text>
+              </View>
+            }
           />
-          
+
           <Card
             heading="Energy - Saving Tips for an eco-Friendly Home"
             bgColor="#E4F1AC"
-            content={newsContent("Reducing energy consumption at home not only lowers utility bills but also helps protect the environment. Simple changes...")}
+            content={
+              <View style={styles.cardTextContent}>
+                <Text style={styles.cardText}>
+                  Reducing energy consumption at home not only lowers utility
+                  bills but also helps protect the environment. Simple
+                  changes...
+                </Text>
+              </View>
+            }
           />
-          
+
           <Card
             heading="Energy - Saving Tips for an eco-Friendly Home"
             bgColor="#E1AFD1"
-            content={newsContent("Reducing energy consumption at home not only lowers utility bills but also helps protect the environment. Simple changes...")}
+            content={
+              <View style={styles.cardTextContent}>
+                <Text style={styles.cardText}>
+                  Reducing energy consumption at home not only lowers utility
+                  bills but also helps protect the environment. Simple
+                  changes...
+                </Text>
+              </View>
+            }
           />
-          
+
           <Card
             heading="Energy - Saving Tips for an eco-Friendly Home"
             bgColor="#F4D9D0"
-            content={newsContent("Reducing energy consumption at home not only lowers utility bills but also helps protect the environment. Simple changes...")}
+            content={
+              <View style={styles.cardTextContent}>
+                <Text style={styles.cardText}>
+                  Reducing energy consumption at home not only lowers utility
+                  bills but also helps protect the environment. Simple
+                  changes...
+                </Text>
+              </View>
+            }
           />
-          
+
           <Card
             heading="Energy - Saving Tips for an eco-Friendly Home"
             bgColor="#F7F9F2"
-            content={newsContent("Reducing energy consumption at home not only lowers utility bills but also helps protect the environment. Simple changes...")}
+            content={
+              <View style={styles.cardTextContent}>
+                <Text style={styles.cardText}>
+                  Reducing energy consumption at home not only lowers utility
+                  bills but also helps protect the environment. Simple
+                  changes...
+                </Text>
+              </View>
+            }
           />
+
         </View>
       </ScrollView>
 
-        {/* Floating add button */}
-        <View style={floatingButtonStyle}>
+        <View style={{
+            width: 80,
+            height: 80,
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            padding: 10,
+        }}
+        >
           <Pressable onPress={() => Alert.alert("Alert", "add")}>
-            <Image source={plus} style={{ width: "100%", height: "100%" }} />
+            <Image source={plus} style={{
+                 width: "100%", 
+                 height: "100%" 
+                 }} 
+                 />
           </Pressable>
         </View>
     </>
@@ -91,3 +184,39 @@ const News = () => {
   return ui;
 };
 
+const styles = StyleSheet.create({
+    container: {
+      width: "100%",
+      alignItems: "center",
+      paddingVertical: 10,
+    },
+    contentContainer: {
+      paddingBottom: 20,
+    },
+    cardContent: {
+      margin: 15,
+    },
+    map: {
+      width: "100%",
+      height: "100%",
+    },
+    infoRow: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginBottom: 10,
+    },
+    icon: {
+      width: 30,
+      height: 30,
+      marginRight: 10,
+    },
+    cardTextContent: {
+      marginTop: 10,
+    },
+    cardText: {
+      fontSize: 16,
+    },
+  });
+  
+  export default News;
+  
