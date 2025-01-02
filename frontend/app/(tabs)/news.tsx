16a7965d96e9c0e7ac398ceb8bda8af8b9a9cbc1
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-02 05:30:32 +0530

    Define news component structure

--- frontend/app/(tabs)/news.tsx
+++ frontend/app/(tabs)/news.tsx
@@ -11,6 +11,16 @@ import {
   View,
 } from "react-native";
 
-
 const plus = require("../../assets/images/plus.png");
 const scope = require("../../assets/images/scope.png");
+
+// News component function
+const News = () => {
+    // UI of the News component
+    const ui = (
+      <>
+        <ScrollView contentContainerStyle={styles.contentContainer}>
+          <View style={styles.container}>
+            <View style={{ width: "100%", padding: 10 }}>
+              <Text style={{ fontSize: 25 }}>Articles/News</Text>
+  
