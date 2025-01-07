0783ac947629ca3ad7c8f0ed3ef259591aed2335
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-07 19:11:40 +0530

    Craete the basic structure of the MapPicker component

---
+++ frontend/components/MapPicker.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const MapPicker = () => {
+  return (
+    <View style={styles.container}>
+      <Text>MapPicker Component</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default MapPicker;
