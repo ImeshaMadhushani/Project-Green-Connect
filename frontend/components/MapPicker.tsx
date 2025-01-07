4a3e7100463583bb609365e48aa2f63389567418
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-07 19:33:19 +0530

    Add actions buttons for saving and cancelling

--- frontend/components/MapPicker.tsx
+++ frontend/components/MapPicker.tsx
@@ -1,36 +1,41 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
-const MapPicker = () => {
+const MapPicker = (
+    { onSaveLocation, onClose }:
+        { onSaveLocation: (location: { lat: number; lng: number }) => void; onClose: () => void }) => {
     const [selectedLocation, setSelectedLocation] = useState<{ lat: number; lng: number } | null>(null);
-  
+
     const handleMapPress = (event: any) => {
-      const { latitude, longitude } = event.nativeEvent.coordinate;
-      setSelectedLocation({ lat: latitude, lng: longitude });
+        const { latitude, longitude } = event.nativeEvent.coordinate;
+        setSelectedLocation({ lat: latitude, lng: longitude });
     };
     return (
         <View style={styles.container}>
-             <MapView
-        style={styles.map}
-        onPress={handleMapPress}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        {selectedLocation && (
-          <Marker
-            coordinate={{
-              latitude: selectedLocation.lat,
-              longitude: selectedLocation.lng,
-            }}
-          />
-        )}
-      </MapView>
-
+            <MapView
+                style={styles.map}
+                onPress={handleMapPress}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            >
+                {selectedLocation && (
+                    <Marker
+                        coordinate={{
+                            latitude: selectedLocation.lat,
+                            longitude: selectedLocation.lng,
+                        }}
+                    />
+                )}
+            </MapView>
+            <View style={styles.actions}>
+                <Button title="Save Location" onPress={() => selectedLocation && onSaveLocation(selectedLocation)} />
+                <Button title="Cancel" color="red" onPress={onClose} />
+            </View>
         </View>
     );
 };
@@ -38,10 +43,16 @@ const MapPicker = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-      },
-      map: {
+    },
+    map: {
         flex: 1,
-      },
+    },
+    actions: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        padding: 10,
+        backgroundColor: "#fff",
+    },
 });
 
 export default MapPicker;
