747283ef787eedad1f21329046536acf2e1d7043
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-07 19:14:46 +0530

    Add MapView to display the map with  an initial region

--- frontend/components/MapPicker.tsx
+++ frontend/components/MapPicker.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import MapView from "react-native-maps";
 
 const MapPicker = () => {
-  return (
-    <View style={styles.container}>
-      <Text>MapPicker Component</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+      },
+      map: {
+        flex: 1,
+      },
 });
 
 export default MapPicker;
