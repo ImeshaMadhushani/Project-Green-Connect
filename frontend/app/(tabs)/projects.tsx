93409c67ed7866f8480a03349972a67ce2cb923e
SanduniAmasha <139688263+SanduniAmasha@users.noreply.github.com>
2025-01-13 12:07:36 +0530

    Add Card Display

--- frontend/app/(tabs)/projects.tsx
+++ frontend/app/(tabs)/projects.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
+import Card from "@/components/Card";
 
 const Projects = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -13,6 +14,24 @@ const Projects = () => {
     <ScrollView contentContainerStyle={styles.contentContainer}>
       <View style={styles.container}>
         <Text style={styles.heading}>Projects</Text>
+        <Card
+            bgColor="#dce8d6"
+            heading="Plastic-Free Market Campaign"
+            content={
+                <View style={styles.cardContent}>
+                <View style={styles.infoRow}>
+                    <Text>November 10, 2024</Text>
+                </View>
+                <View style={styles.infoRow}>
+                    <Text>9:00 a.m.</Text>
+                </View>
+                <View style={styles.infoRow}>
+                    <Text>Vavuniya, Market</Text>
+                </View>
+                </View>
+            }
+         />
+
       </View>
     </ScrollView>
   );
@@ -30,6 +49,14 @@ const styles = StyleSheet.create({
   heading: {
     fontSize: 25,
     padding: 10,
+  },
+  cardContent: {
+    margin: 15,
+  },
+  infoRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
   },
 });
 
