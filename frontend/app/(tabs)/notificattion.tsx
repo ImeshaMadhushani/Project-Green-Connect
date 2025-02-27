c7b25413ea9874868df9542759ca7980539d3677
SanduniAmasha <139688263+SanduniAmasha@users.noreply.github.com>
2025-02-27 19:33:14 +0530

    fix some typos in notifications

--- frontend/app/(tabs)/notificattion.tsx
+++ frontend/app/(tabs)/notificattion.tsx
@@ -16,39 +16,7 @@ const Home=()=>{
             ><Text>Content</Text></CardReg>
             <CardReg heading="Heading" bgColor="#FFFDEC"
             ><Text>Content</Text></CardReg>
-        </View>import CardReg from "@/components/CardReg";
-import { ScrollView, StyleSheet, Text, View } from "react-native"
-
-const Home=()=>{
-    const ui=(
-    <ScrollView contentContainerStyle={{paddingBottom: 20,}}>
-        <View style={styles.container}>
-            <Text style={{fontSize:30,marginTop:10}}>Notifications</Text>
-            <CardReg heading="Heading" bgColor="#FFFDEC"
-            ><Text>Content</Text></CardReg>
-            <CardReg heading="Heading" bgColor="#FFFDEC"
-            ><Text>Content</Text></CardReg>
-            <CardReg heading="Heading" bgColor="#FFFDEC"
-            ><Text>Content</Text></CardReg>
-            <CardReg heading="Heading" bgColor="#FFFDEC"
-            ><Text>Content</Text></CardReg>
-            <CardReg heading="Heading" bgColor="#FFFDEC"
-            ><Text>Content</Text></CardReg>
-        </View> */}
-    </ScrollView>
-    );
-    return ui;
-}
-
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:'center',
-    },
-    
-});
-
-export default Home;
+        </View>
     </ScrollView>
     );
     return ui;
