22a908aecc248d65e45e1d17b467db12f524f066
SanduniAmasha <139688263+SanduniAmasha@users.noreply.github.com>
2025-02-28 22:50:01 +0530

    Edit profile image edit design do

--- frontend/app/view/editProfile.tsx
+++ frontend/app/view/editProfile.tsx
@@ -1,8 +1,12 @@
 import ButtonSuccess from "@/components/button-success";
 import TextInputStyled from "@/components/text-input";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View, TouchableOpacity} from "react-native";
 import { Image } from "react-native-elements";
-//const user = require("@/assets/images/user.png");
+import * as ImagePicker from "expo-image-picker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const user = require("@/assets/images/user.png");
 
 const EditProfile = () => {
   const ui = (
@@ -16,68 +20,95 @@ const EditProfile = () => {
           gap: 10,
         }}
       >
-      
-              <View
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  width: "100%",
-                  gap: 10,
-                  padding: 10,
-                }}
-              >
-                {/* <Image
-                  style={{
-                    width: 120,
-                    aspectRatio: 1,
-                  }}
-                  source={user}
-                /> */}
-                <TextInputStyled
-                  placeholder="First Name"
-                  value=""
-                  text="First Name"
-                />
-                <TextInputStyled
-                  placeholder="Last Name"
-                  value=""
-                  text="Last Name"
-                />
-      
-                <TextInputStyled
-                  placeholder="E-mail"
-                  value=""
-                  text="E-mail"
-                />
-                <TextInputStyled
-                  placeholder="District"
-                  value=""
-                  text="District"
-                />
-                <TextInputStyled
-                  placeholder="Area"
-                  value=""
-                  text="Area"
-                />
-                <ButtonSuccess  label="Edit Profile" />
-              </View>
-            </View>
-            <View style={{ padding: 10, marginTop: 10 }}></View>
-          </ScrollView>
-        );
-        return ui;
-      };
-      
-      const style = StyleSheet.create({
-        container: {
-          flexGrow: 1,
-          padding: 10,
-        },
-        text: {},
-      });
-      
-      export default EditProfile;
-      
+
+        <View
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            gap: 10,
+            padding: 10,
+          }}
+        >
+         <TouchableOpacity style={style.imageContainer}>
+          <Image
+            style={style.profileImage}
+            source={ user}
+          />
+          <View style={style.iconContainer}>
+            <Icon name="camera" size={20} color="white" />
+          </View>
+        </TouchableOpacity>
+
+          <TextInputStyled
+            placeholder="First Name"
+            value=""
+            text="First Name"
+          />
+          <TextInputStyled
+            placeholder="Last Name"
+            value=""
+            text="Last Name"
+          />
+          <TextInputStyled
+            placeholder="User Name"
+            value=""
+            text="User Name"
+          />
+          <TextInputStyled
+            placeholder="E-mail"
+            value=""
+            text="E-mail"
+          />
+          <TextInputStyled
+            placeholder="District"
+            value=""
+            text="District"
+          />
+          <TextInputStyled
+            placeholder="Area"
+            value=""
+            text="Area"
+          />
+          <ButtonSuccess label="Edit Profile" />
+        </View>
+      </View>
+      <View style={{ padding: 10, marginTop: 10 }}></View>
+    </ScrollView>
+  );
+  return ui;
+};
+
+const style = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: 10,
+  },
+  text: {},
+  imageContainer: {
+    position: "relative",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profileImage: {
+    width: 120,
+    aspectRatio: 1,
+    borderRadius: 60,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  iconContainer: {
+    position: "absolute",
+    bottom: 5,
+    right: 5,
+    backgroundColor: "#004d00",
+    borderRadius: 20,
+    padding: 5,
+  },
+});
+
+export default EditProfile;
+
 
 
