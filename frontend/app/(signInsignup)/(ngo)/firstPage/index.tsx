dc576c2e8f0b5ea369e88be22ca3168eead440a8
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2025-02-28 14:41:08 +0530

    fix organization  validation

--- frontend/app/(signInsignup)/(ngo)/firstPage/index.tsx
+++ frontend/app/(signInsignup)/(ngo)/firstPage/index.tsx
@@ -4,17 +4,13 @@ import { TextInput } from "react-native-gesture-handler";
 import TextInputStyled from "@/components/text-input";
 import { router } from "expo-router";
 import ButtonSuccess from "@/components/button-success";
+import ButtonText from "@/components/button-text";
 
 const First = () => {
   const [ngoname, setNgoname] = useState("");
-<<<<<<< HEAD
   const [regNo, setRegno] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
-=======
-  const [uniqueId, setUniqueId] = useState("");
-  const [registrationDate, setRegistrationDate] = useState("");
->>>>>>> af921d0a778beb760a202639b0dee99fd8fe98ca
 
   const input1Ref = useRef<TextInput>(null);
   const input2Ref = useRef<TextInput>(null);
@@ -52,7 +48,6 @@ const First = () => {
     });
   };
 
-<<<<<<< HEAD
   return (
     <View style={styles.container}>
     {/*   <View style={styles.stepContainer}>
@@ -68,70 +63,10 @@ const First = () => {
         value={ngoname}
         placeholder="Enter the name of your NGO"
       />
-=======
-
-  
-
-  const ui = (
-    <View
-      style={{
-        flex: 1,
-        width: "100%",
-        padding: 20,
-        alignItems: "center",
-      }}
-    >
-      <View
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          gap: 10,
-          margin: 10,
-        }}
-      >
-        <View
-          style={{
-            width: 15,
-            height: 15,
-            borderColor: "#0D7C66",
-            borderWidth: 2,
-            borderRadius: "100%",
-            backgroundColor: "#0D7C66",
-          }}
-        />
-        <View
-          style={{
-            width: 15,
-            height: 15,
-            borderColor: "#0D7C66",
-            borderWidth: 2,
-            borderRadius: "100%",
-          }}
-        />
-        <View
-          style={{
-            width: 15,
-            height: 15,
-            borderColor: "#0D7C66",
-            borderWidth: 2,
-            borderRadius: "100%",
-          }}
-        />
-      </View>
-      <TextInputStyled
-          ref={input1Ref}
-          returnKeyType="next"
-          text="Organization Name"
-          onChangeText={setNgoname}
-          value={ngoname}
-          placeholder={"Enter the name of your NGO"}
-        />
->>>>>>> af921d0a778beb760a202639b0dee99fd8fe98ca
       <TextInputStyled
         ref={input2Ref}
         returnKeyType="next"
         text="Registration No"
-<<<<<<< HEAD
         onChangeText={setRegno}
         value={regNo}
         placeholder="Enter the reg no"
@@ -170,60 +105,17 @@ const First = () => {
           onPress={handleNext}
         />
       </View>
-=======
-        onChangeText={setUniqueId}
-        value={uniqueId}
-        placeholder={"Enter the reg no"}
-      />
-      <TextInputStyled
-        ref={input3Ref}
-        returnKeyType="done"
-        text="Registration Date"
-        onChangeText={setRegistrationDate}
-        value={registrationDate}
-        placeholder="Enter the reg date"
-      />
-  
-      <View style={{ flex: 1, flexDirection: "row",
-          justifyContent: "space-between",
-          marginTop: 20,
-          width: "100%", }}>
-        <ButtonSuccess
-          style={{ width: 100, height: 45,}}
-          label="Back"
-          onPress={() => {
-            router.navigate("/", { relativeToDirectory: true });
-          }}
-        />
-      <ButtonSuccess
-        label="Next"
-        style={{ width: 100, height: 45,}}
-        onPress={() => {
-          /* router.navigate("/secondPage", { relativeToDirectory: true }); */
-          handleNext();
-        }}
-      />
-    </View>
->>>>>>> af921d0a778beb760a202639b0dee99fd8fe98ca
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    alignItems: "center",
-<<<<<<< HEAD
-    backgroundColor: "#F9F9F9",
-=======
-    width: "100%",
-    paddingHorizontal: 16,
-  },
-  divider: {
     flex: 1,
-    height: 1,
-    backgroundColor: "#ccc",
->>>>>>> af921d0a778beb760a202639b0dee99fd8fe98ca
+    width: "100%",
+    padding: 20,
+    alignItems: "center",
+    backgroundColor: "#F9F9F9",
   },
   stepContainer: {
     flexDirection: "row",
