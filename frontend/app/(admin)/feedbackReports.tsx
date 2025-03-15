802a053ac7f815e9affa62eb4fad5b49da9af0c1
manimekala123 <139643976+manimekala123@users.noreply.github.com>
2025-03-15 09:28:26 +0530

    Add MaterialCommunityIcons and Pressable Imports to feedbackreports component

--- frontend/app/(admin)/feedbackReports.tsx
+++ frontend/app/(admin)/feedbackReports.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet,Pressable, Alert } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const feedbackData = [
   { id: "1", user: "John Doe", feedback: "Great app! Easy to use.", rating: 5 },
