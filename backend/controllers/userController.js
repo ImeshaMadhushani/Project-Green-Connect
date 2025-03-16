ed4f8cab6fc946d4b9dc53310b1f67bde569e34c
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2025-03-16 11:50:50 +0530

    create function to get approve organization

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -484,3 +484,14 @@ export async function logout(req, res) {
     }
 }
 
+//get approve organizations
+export async function getApproveOrganizations(req, res) {
+    try {
+        const organizations = await User.find({ role: 'organization', isApproved: true });
+        res.status(200).json({ message: 'Organizations fetched successfully', organizations });
+    }
+    catch (error) {
+        console.error("Error fetching organizations:", error);
+        res.status(500).json({ message: 'Error fetching organizations', error: error.message });
+    }
+}
