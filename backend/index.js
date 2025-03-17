238cd56434dccff2d743fbcee6ad949ad42f474a
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2025-03-17 18:12:41 +0530

    fix errors

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ import methodOverride from 'method-override';
 import  path  from 'path';
 import { fileURLToPath } from 'url';
 import morgan from 'morgan';
+import feedbackRouter from './routes/feedbackRoute.js';
 
 
 
@@ -74,7 +75,7 @@ app.use((req, res, next) => {
 app.use("/api/user", userRouter);
 app.use("/api/project", projectRouter);
 app.use('/api/post', postRoute)
-
+app.use ('/api/feedback', feedbackRouter)
 
 
 // Connect to MongoDB
